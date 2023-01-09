0683a4c6e1cfae0a52b4102b1f80e3102172ae11
rabbani4u <rabbani.iu31@gmail.com>
2023-01-09 16:38:39 +0600

    auth

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,8 +1,3 @@
-import React from "react";
-import "./App.css";
-import { initializeApp } from "firebase/app";
-
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCYlEWkWngXs1GeMAmEyiI4GjYKwvad7rI",
   authDomain: "ema-jhon-simple-be3f1.firebaseapp.com",
@@ -11,11 +6,4 @@ const firebaseConfig = {
   messagingSenderId: "469552364960",
   appId: "1:469552364960:web:20912b251eaa3e1923f8c5",
 };
-
-const app = initializeApp(firebaseConfig);
-
-function App() {
-  return <div></div>;
-}
-
-export default App;
+export default firebaseConfig;
